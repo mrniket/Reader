3b3844882286871b67e7e2af6ca4a8fe92f5d8c9
Niket Shah <masterniket@gmail.com>
2015-03-31 17:03:29 +0100

    add global reader configuration class

---
+++ Reader/ReaderConfig.swift
@@ -0,0 +1,12 @@
+//
+//  ReaderConfig.swift
+//  Reader
+//
+//  Created by Niket Shah on 31/03/2015.
+//  Copyright (c) 2015 Niket Shah. All rights reserved.
+//
+
+
+struct ReaderConfig {
+    static var pdfLibraryPath = "/Users/Niket/Desktop/ReaderFiles/"
+}
