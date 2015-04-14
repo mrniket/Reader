fb301855f6b33a0d494bf48c02230a762830098e
Niket Shah <masterniket@gmail.com>
2015-04-14 12:28:12 +0100

    Add AuthenticatorType and MendeleyAuthenticator

--- Reader/AuthenticatorType.swift
+++ Reader/AuthenticatorType.swift
@@ -6,4 +6,12 @@
 //  Copyright (c) 2015 Niket Shah. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+
+protocol AuthenticatorType: class {
+	
+	func isAuthenticated() -> Bool
+	func getLoginWindowController() -> NSWindowController
+	func clearAuthentication()
+	
+}
--- Reader/MendeleyAuthenticator.swift
+++ Reader/MendeleyAuthenticator.swift
@@ -6,4 +6,38 @@
 //  Copyright (c) 2015 Niket Shah. All rights reserved.
 //
 
-import Foundation
+import Cocoa
+import MendeleyKit
+
+class MendeleyAuthenticator: AuthenticatorType {
+	
+	init() {
+		MendeleyKitConfiguration.sharedInstance().configureOAuthWithParameters(MendeleyConfig.asDictionary())
+		let networkProviderParameters = [kMendeleyNetworkProviderKey: MendeleyNSURLConnectionProvider.className()]
+		MendeleyKitConfiguration.sharedInstance().changeConfigurationWithParameters(networkProviderParameters)
+	}
+	
+	func isAuthenticated() -> Bool {
+		return MendeleyKit.sharedInstance().isAuthenticated
+	}
+	
+	func getLoginWindowController() -> NSWindowController {
+		var loginViewController: MendeleyLoginWindowController? = nil
+		loginViewController =  MendeleyLoginWindowController(clientKey: MendeleyConfig.clientID, clientSecret: MendeleyConfig.secret, redirectURI: MendeleyConfig.redirectURL, completionBlock: { (sucess, error) -> Void in
+			//                self.window?.endSheet(self.loginController!.window!)
+			loginViewController!.window!.sheetParent?.endSheet(loginViewController!.window!)
+//			NSApp.endSheet(loginViewController!.window!)
+			println("authenticated via login")
+			println(sucess)
+			if error != nil {
+				println(error)
+			}
+		})
+		return loginViewController!
+	}
+	
+	func clearAuthentication() {
+		MendeleyKit.sharedInstance().clearAuthentication()
+	}
+	
+}
