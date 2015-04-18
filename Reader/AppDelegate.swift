5d202276b8a97d15ef55374029007ffea6fb7848
Niket Shah <masterniket@gmail.com>
2015-04-18 09:01:17 +0100

    shouldn't show an untitled file when application opens

--- Reader/AppDelegate.swift
+++ Reader/AppDelegate.swift
@@ -21,6 +21,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+	func applicationShouldOpenUntitledFile(sender: NSApplication) -> Bool {
+		return false
+	}
 
 }
 
