2c53068ac082ec2890b386a90ae84716315b358c
Niket Shah <masterniket@gmail.com>
2015-05-31 10:35:00 +0100

    use webview instead of SVGKit

--- Reader/CollectionViewDetailViewController.swift
+++ Reader/CollectionViewDetailViewController.swift
@@ -7,22 +7,29 @@
 //
 
 import Cocoa
-import SVGKit
+import WebKit
 
 class CollectionViewDetailViewController: NSViewController {
 
 
+	@IBOutlet weak var webView: WebView!
 	
-	@IBOutlet weak var imageView: SVGKLayeredImageView!
 	
 	var svg: String = "" {
 		didSet {
-			let source = SVGKSource(contentsOfString: svg)
-			let image = SVGKImage(source: source)
-			image.size = imageView.bounds.size
-			imageView.image = image
+			let svgWithDimensions = svg.stringByReplacingOccurrencesOfString("<svg", withString: "<svg width=\"800px\" height=\"800px\"", options: NSStringCompareOptions.allZeros, range: nil)
+
+			webView.mainFrame.frameView.documentView.scaleUnitSquareToSize(NSMakeSize(1.6, 1.6))
+			webView.mainFrame.loadHTMLString(svgWithDimensions, baseURL: ReaderConfig.pdfLibraryPath)
+
+//			let source = SVGKSource(contentsOfString: svg)
+//			let image = SVGKImage(source: source)
+//			image.size = imageView.bounds.size
+//			imageView.image = image
 		}
 	}
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
